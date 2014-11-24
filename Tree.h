db6ac9868077ff893515bef2716d69c35afc32d7
IgnacioC <contrerasign@gmail.com>
2014-11-24 07:10:53 -0600

    Commit: is working with three different examples.

--- Tree.h
+++ Tree.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 
 using namespace std;
@@ -22,7 +23,7 @@ public:
 
     Node<T> *getParent() const;
     void setParent(Node *value);
-    int getData();
+    T getData();
     void setData(T value);
     vector<Node<T> *> *getChilds();
     Node<T> *getChildByPos(int pos);
@@ -61,7 +62,7 @@ void Node<T>::setParent(Node *value)
 }
 
 template<class T>
-int Node<T>::getData()
+T Node<T>::getData()
 {
     return this->data;
 }
@@ -110,6 +111,7 @@ class Tree
 private:
     Node<U> *root;
     Node<U> *current;
+    string filename;
 
 public:
     Tree();
@@ -117,13 +119,30 @@ public:
     vector<Node<U> *> *insert(Node<U> *ref, vector<U> data);
     void preorder(Node<U> *n);
     void posOrder(Node<U> *n);
+//    void inOrder(Node<U> *n);
 
     Node<U> *getRoot() const;
     void setRoot(Node<U> *value);
     Node<U> *getCurrent() const;
     void setCurrent(Node<U> *value);
+    void createGraph(Node<U> *n);
+    void preOrder(Node<U> *n);
+    string getFilename() const;
+    void setFilename(const string &value);
 };
 
+
+template<class U>
+string Tree<U>::getFilename() const
+{
+    return filename;
+}
+
+template<class U>
+void Tree<U>::setFilename(const string &value)
+{
+    filename = value;
+}
 
 template<class U>
 Tree<U>::Tree():root(NULL),current(NULL)
@@ -154,6 +173,8 @@ void Tree<U>::setCurrent(Node<U> *value)
 {
     current = value;
 }
+
+
 
 template<class U>
 Node<U> *Tree<U>::init(U data)
@@ -213,8 +234,69 @@ void Tree<U>::posOrder(Node<U> *n)
     cout<<n->getData()<<endl;
 }
 
+/*
+template<class U>
+void Tree<U>::inOrder(Node<U> *n)
+{
+    if(n=NULL) return;
+
+    for(int i =0; i<n->getChildSize();i++)
+    {
+        inOrder(n->getChildByPos(i));
+        cout<<n->getData()<<endl;
+        inOrder(n->getChildByPos(i));
+    }
 
 
+}
+*/
+
+template<class U>
+void Tree<U>::createGraph(Node<U> *n)
+{
+    ofstream file;
+    file.open(this->getFilename().c_str());
+    if(file.is_open())
+    {
+       file<<"graph{ "<<endl;
+
+       for(int i=0; i<n->getChildSize();i++)
+       {
+           file<<n->getData()<<" -- ";
+           preOrder(n->getChildByPos(i));
+
+       }
+
+//    file<<"}"<<endl;
+     file.close();
+
+    }
+
+
+}
+template<class U>
+void Tree<U>::preOrder(Node<U> *n)
+{
+    ofstream file;
+    file.open(this->getFilename().c_str(), ios::app);
+    if(file.is_open())
+    {
+    if(n==NULL){ file<<"\n"; return;}
+
+      file<<n->getData()<<endl;
+    for(int i=0; i<n->getChildSize();i++)
+    {
+        file<<n->getData()<<" -- ";
+        preOrder(n->getChildByPos(i));
+
+    }
+
+     file.close();
+    }
+
+
+
+}
 
 
 #endif // TREE_H
--- main.cpp
+++ main.cpp
@@ -2,34 +2,104 @@
 
 int main()
 {
+/*
+    string filename;
+    Tree<string> t1;
+    Node<string> *aux;
+    vector<Node<string>*> *aux2;
+    vector<string> num;
 
+    num.push_back("Fate");
+    num.push_back("Destiny");
+    aux = t1.init("Life");
+    aux2 = t1.insert(aux,num);
+
+    num.clear();
+    num.push_back("Doom");
+    num.push_back("Work");
+    num.push_back("Money");
+    t1.insert(aux2->at(0),num);
+
+    num.clear();
+
+    num.push_back("Hallelujah");
+    num.push_back("Child");
+    num.push_back("Konami");
+    num.push_back("Contra");
+
+    t1.insert(aux2->at(1),num);
+    cout<<"Tree with Strings:"<<endl;
+    cout<<"Pre-Order"<<endl;
+    t1.preorder(aux);
+    cout<<endl;
+    cout<<"Post-Order"<<endl;
+    t1.posOrder(aux);
+*/
+/*
+    Tree<double> t1;
+    Node<double> *aux;
+    vector<Node<double>*> *aux2;
+    vector<double> num;
+
+    num.push_back(15.5);
+    num.push_back(42.42);
+    aux = t1.init(3.1456);
+    aux2 = t1.insert(aux,num);
+
+    num.clear();
+    num.push_back(3.2);
+    num.push_back(5.3);
+    num.push_back(10.01);
+    t1.insert(aux2->at(0),num);
+
+    num.clear();
+
+    num.push_back(68.36);
+    num.push_back(52.36);
+    num.push_back(485.35);
+    num.push_back(156.32);
+
+    t1.insert(aux2->at(1),num);
+    cout<<"Tree with Double:"<<endl;
+    cout<<"Pre-Order"<<endl;
+    t1.preorder(aux);
+    cout<<endl;
+    cout<<"Post-Order"<<endl;
+    t1.posOrder(aux);
+*/
     Tree<int> t1;
     Node<int> *aux;
     vector<Node<int>*> *aux2;
     vector<int> num;
 
-    num.push_back(2);
-    num.push_back(3);
-    aux = t1.init(1);
+    num.push_back(15);
+    num.push_back(42);
+    aux = t1.init(3);
     aux2 = t1.insert(aux,num);
 
     num.clear();
-    num.push_back(4);
+    num.push_back(3);
     num.push_back(5);
-    num.push_back(99);
+    num.push_back(10);
     t1.insert(aux2->at(0),num);
 
     num.clear();
-    num.push_back(6);
-    num.push_back(7);
-    num.push_back(77);
-    num.push_back(66);
+
+    num.push_back(68);
+    num.push_back(52);
+    num.push_back(4);
+    num.push_back(1);
 
     t1.insert(aux2->at(1),num);
-    cout<<"PreOrden"<<endl;
+    cout<<"Tree with Double:"<<endl;
+    cout<<"Pre-Order"<<endl;
     t1.preorder(aux);
-    cout<<"PostOrden"<<endl;
+    cout<<endl;
+    cout<<"Post-Order"<<endl;
     t1.posOrder(aux);
+    cout<<endl;
+
+
 
     return 0;
 }
